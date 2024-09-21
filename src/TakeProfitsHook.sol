4d8cc17a9e46e96b07f67db68098dd8d6939165d
efe <e.sozen@student.tudelft.nl>
2024-09-21 14:18:36 +0200

    :sparkles: initalization

---
+++ src/TakeProfitsHook.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {BaseHook} from "v4-periphery/src/base/hooks/BaseHook.sol";
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+
+contract TakeProfitsHook is BaseHook, ERC1155 {
+    // Initialize BaseHook and ERC1155 parent contracts in the constructor
+    constructor(
+        IPoolManager _poolManager,
+        string memory _uri
+    ) BaseHook(_poolManager) ERC1155(_uri) {}
+
+    // Required override function for BaseHook to let the PoolManager know which hooks are implemented
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
+    {
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: true,
+                beforeAddLiquidity: false,
+                afterAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: false,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false
+            });
+    }
+}
